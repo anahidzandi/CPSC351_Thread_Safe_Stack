f76fa317c41028d177782b0b70cce0e3bae1c9dc
anazandi78 <anazandi78@gmail.com>
2022-11-27 14:07:10 -0800

    initial commit

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+executableFileName="${1:-stack}"
+
+
+# Find and display all the c source files to be compiled ...
+# temporarily ignore spaces when globing words into file names
+temp=$IFS
+  IFS=$'\n'  
+  sourceFiles=( $(find ./ -name "*.c") )
+IFS=$temp
+
+echo "compiling ..."
+for fileName in "${sourceFiles[@]}"; do
+  echo "  $fileName"
+done
+echo ""
+
+# Set some options and perform the compolations ...
+options="-g3 -O0 -pthread  -I./ -DUSING_TOMS_SUGGESTIONS"
+# options="-g0 -O3 -DNDEBUG -pthread  -I./ -DUSING_TOMS_SUGGESTIONS"
+gcc $options -o "$executableFileName" "${sourceFiles[@]}" -lrt && echo -e "\nSuccessfully created  \"$executableFileName\""
---
+++ stack.c
@@ -0,0 +1,111 @@
+#include <stdlib.h>  // NULL, malloc(), free()
+#include <assert.h>  // assert()
+#include <pthread.h>
+#include "stack.h"
+
+
+
+/*******************************************************************************
+** Public types and objects definitions
+*******************************************************************************/
+const Stack EmptyStack = {NULL, 0, PTHREAD_MUTEX_INITIALIZER};
+
+struct StackNode
+{
+  value_t            _data;
+  struct StackNode * _next;
+};
+
+
+
+/*******************************************************************************
+** Private helper functions
+*******************************************************************************/
+static int isValid( Stack * stack )
+{
+  // verify stack really points to something, presumably a stack
+  assert( stack );
+
+  // Verify the number of nodes in the stack matches the stack's size
+  unsigned count = stack->_size;
+
+  for(StackNode * current = stack->_top;  current;  current = current->_next)  --count;
+
+  return count == 0;
+}
+
+
+
+/*******************************************************************************
+** Public function definitions
+*******************************************************************************/
+void push( Stack * stack, value_t data )
+{
+
+  pthread_mutex_lock(&stack->_mutex);
+
+  // To help detect stack integrity problems, verify consistency between "top" and "size" attributes
+  assert( stack && isValid(stack) );
+
+  // provision memory for a new node
+  StackNode * newNode = malloc( sizeof(StackNode) );
+
+  // populate the new node with data
+  newNode->_data = data;
+
+  // link the new node onto the top of the stack and advance the stack top
+  newNode->_next = stack->_top;
+  stack->_top    = newNode;
+
+  // maintain stack size
+  ++stack->_size;
+  pthread_mutex_unlock(&stack->_mutex);
+}
+
+
+
+value_t pop( Stack * stack )
+{
+
+  pthread_mutex_lock(&stack->_mutex);
+
+  // To help detect stack integrity problems, verify consistency between "top" and "size" attributes
+  assert( stack && isValid(stack) );
+  
+  // What to do if asked to return data from an empty stack?
+  if( isEmpty( stack ) ) // is empty?
+  {
+    pthread_mutex_unlock(&stack->_mutex);
+    return (value_t)0;
+  }
+
+  pthread_mutex_unlock(&stack->_mutex);
+
+  pthread_mutex_lock(&stack->_mutex);
+  // advance the top of the stack to the next element
+  StackNode * temp = stack->_top;
+  stack->_top      = stack->_top->_next;
+
+  // maintain stack size
+  --stack->_size;
+  pthread_mutex_unlock(&stack->_mutex);
+
+  // hold a copy of the data at the top of the stack
+  value_t data = temp->_data;
+
+  // release the popped node
+  free( temp );
+
+  // return the requested data
+  return data;
+}
+
+
+
+int isEmpty( Stack * stack )
+{
+  // To help detect stack integrity problems, verify consistency between "top" and "size" attributes
+  assert( stack && isValid(stack) );
+
+  return stack->_top == NULL;
+}
